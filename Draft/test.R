72f53cbdf3dff21d0cfa0bc27cc29d9f80dc99c8
Ouyang <oxu2@wisc.edu>
2021-12-07 21:01:12 -0600

    code

---
+++ Draft/test.R
@@ -0,0 +1,6 @@
+# word="refund"
+# print(data_2 %>%
+#         filter(word.stem == word) %>%
+#         ggplot()+
+#         geom_bar(aes(x = star_rating,y = frequency,fill = star_rating),stat = "identity")+
+#         ggtitle(word))
