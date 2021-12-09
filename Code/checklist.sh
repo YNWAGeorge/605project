d53e9f4f22e548d0c88aef4565578d237c53a5e4
Ouyang <oxu2@wisc.edu>
2021-12-08 19:40:01 -0600

    Revert "Add files via upload"
    This reverts commit da691183543e0e83e5e717ffd7b5dd1ee11e6e36.

--- Code/checklist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-name=$1
-find . -name "$1[0-9][0-9]" > file_list 
--- Code/save.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -name "$1[0-9][0-9].csv" -exec tar -rvf $1.tar {} \;
-cp $1.tar /home/txu98/
-
-
